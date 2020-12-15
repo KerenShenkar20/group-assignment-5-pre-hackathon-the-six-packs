1d4141d0ff2c88a9ae1c15f75b15b693ecd5d79b
Yamarbel <37666905+Yamarbel@users.noreply.github.com>
2020-12-15 20:43:27 +0200

    yams routing

--- service/index.js
+++ service/index.js
@@ -30,3 +30,48 @@ app.use('/users', usersRouter);
 //Listening on port 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(colors.red.underline.bgBrightWhite(`Server running on port ${port}`)));
+
+const datapath = require('./data/data.json')
+
+app.post('/user/create', (req, res) => {
+    
+    createUser(req, res)
+})
+
+app.get('/user/read:id', (req,res) => {
+
+    readUser(req, res)
+})
+
+
+const readUser = (req, res) => {
+
+    const usersList = datapath;
+    res.send({sucsess: true, usersList})
+}
+
+app.put('/user/update:id', (req, res) => {
+    updateUser(req, res)
+})
+
+const updateUser = (req, res) => {
+    const usersList = datapath
+    if (req.params.id < 0 || req.params.id > 500) {
+        return res.send({error: true, msg: 'User not exist'})
+    }
+    const id = req.params.id
+    usersList[id].first_name = req.params.first_name
+    usersList[id].last_name = req.params.last_name
+    usersList[id].email = req.params.email
+    usersList[id].country = req.params.country
+    usersList[id].avatar = req.params.avatar
+    usersList[id].color = req.params.color   
+    usersList[id].job = req.params.job 
+    usersList.push(usersList[id])
+    res.send({success: true, msg: 'User data updated successfully'})
+}
+
+app.delete('/user/delete:id', (req, res) => {
+    deleteUser(req, res)
+})
+//test
